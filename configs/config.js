306d1be44e5ca552b890468d8c04279890d08a15
vinhphan812 <vinhphan812@gmail.com>
2022-11-01 18:47:56 +0700

    sync

--- configs/config.js
+++ configs/config.js
@@ -4,29 +4,29 @@ const redis = require("redis");
 const { db_url, db_user, db_pass, db_name, redisURL, redisKey } = process.env;
 
 const redisClient = redis.createClient({
-	url: redisURL,
-	password: redisKey,
+     url: redisURL,
+     password: redisKey,
 });
 
 redisClient.on("error", (err) => {
-	console.log("REDIS ERROR - " + err);
+     console.log("REDIS ERROR - " + err);
 });
 
 redisClient.on("connect", (stream) => {
-	console.log("Redis CONNECTED...");
+     console.log("Redis CONNECTED...");
 });
 
 redisClient.connect();
 
 module.exports = {
-	initDatabase: () => {
-		// connect database from .env
-		if (db_url)
-			mongoose.connect(db_url, {
-				user: db_user,
-				pass: db_pass,
-				dbName: db_name,
-			});
-	},
-	redisClient,
+     initDatabase: () => {
+          // connect database from .env
+          if (db_url)
+               mongoose.connect(db_url, {
+                    user: db_user,
+                    pass: db_pass,
+                    dbName: db_name,
+               });
+     },
+     redisClient,
 };
