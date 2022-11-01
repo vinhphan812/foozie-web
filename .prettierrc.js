d6b6ffaa84b2e7191f43dc82b6d76620c41d219d
vinhphan812 <vinhphan812@gmail.com>
2022-11-01 19:56:10 +0700

    update prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
      tabWidth: 5,
-     semi: false,
-     singleQuote: true,
+     semi: true,
+     singleQuote: false,
      trailingComma: "es5",
      overrides: [
           {
