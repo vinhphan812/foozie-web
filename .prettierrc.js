fbe7841940003db2a71f23c0657e1836f1d244a7
vinhphan812 <vinhphan812@gmail.com>
2022-11-01 18:47:18 +0700

    update .prettierrc for src

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+     tabWidth: 5,
+     semi: false,
+     singleQuote: true,
+     trailingComma: "es5",
+     overrides: [
+          {
+               files: "*.css",
+               options: {
+                    tabWidth: 4,
+               },
+          },
+     ],
+};
