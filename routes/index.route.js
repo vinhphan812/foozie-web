b03bcd14058c206e1e8fdb899b287098b8a6faaf
Pham Dang Khoa <19dh111073@st.huflit.edu.vn>
2022-11-06 13:12:29 +0700

    list food by type

--- routes/index.route.js
+++ routes/index.route.js
@@ -5,12 +5,14 @@ const AuthRoute = require("./auth.route");
 const UserRoute = require("./user.route");
 const FoodRoute = require("./food.route");
 const AdminRoute = require("./admin/index.route");
+const PublicRoute = require("./public.route");
 
 const router = Router();
 
 router.use(AuthRoute);
 router.use("/admin", AdminRoute);
 router.use("/user", UserRoute);
+router.use("/", PublicRoute);
 router.use("/foods", FoodRoute);
 router.get("/search", searchPage);
 router.get("/", homePage);
